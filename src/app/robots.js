60a5e8db72d4ea784db77a85304fffaf2c835eb0
Dr Bhuvanesh <ybhuvan7@gmail.com>
2026-07-09 11:55:43 +0530

    Add robots.txt

---
+++ src/app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://drbhuvandentalcare.com/sitemap.xml",
+    host: "https://drbhuvandentalcare.com",
+  };
+}
