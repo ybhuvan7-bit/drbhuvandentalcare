2c658668580c8c2be806eb2db4dde1aea82e3a57
Dr Bhuvanesh <ybhuvan7@gmail.com>
2026-07-09 11:30:41 +0530

    Fix layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,11 +20,16 @@ export const metadata = {
     google: "Ko0cMgfj1sO9bXk8DVzOzeeQv3M3ytRZOJfi6OENsec",
   },
 };
+
+export default function RootLayout({ children }) {
+  return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
