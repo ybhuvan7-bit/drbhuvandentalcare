7733d5ed187357fe41859077fe03e66f3ce77da7
Dr Bhuvanesh <ybhuvan7@gmail.com>
2026-07-09 17:27:27 +0530

    Premium transparent navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,20 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white/95 backdrop-blur-md shadow-lg z-50">
+    <nav
+  className={`fixed top-0 left-0 w-full z-50 transition-all duration-500 ${
+    scrolled
+      ? "bg-white/95 backdrop-blur-md shadow-xl py-0"
+      : "bg-transparent py-2"
+  }`}
+>
 
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
 
         <div>
-          <h1 className="text-2xl font-bold text-orange-600">
+          <h1 className={`text-2xl font-bold transition ${
+  scrolled ? "text-orange-600" : "text-white"
+}`}>
             🦷 Dr. Bhuvan's
           </h1>
 
-          <p className="text-xs text-gray-500">
+          <p className={`text-xs transition ${
+  scrolled ? "text-gray-500" : "text-gray-200"
+}`}>
             Dental Laser & Implant Centre
           </p>
         </div>
 
-        <ul className="hidden lg:flex gap-8 text-gray-700 font-semibold">
+        <ul className={`hidden lg:flex gap-8 font-semibold transition ${
+  scrolled ? "text-gray-700" : "text-white"
+}`}>
 
           <li><a href="#">Home</a></li>
 
